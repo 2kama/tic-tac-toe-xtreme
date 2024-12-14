bd7e6bf104c2585544d2c14f6d15f25340609428
Mishael Kama <mishaelkama@Mishaels-MacBook-Pro.local>
2024-12-14 12:59:10 -0500

    feat: personalize gameplay info

--- app/game/[gameid]/Outcome.tsx
+++ app/game/[gameid]/Outcome.tsx
@@ -13,19 +13,19 @@ const Outcome = ({ gameData }: Props) => {
   const O_Won = gameData.outCome.toLowerCase().includes("o won");
 
   return (
-    <div className="flex w-[80%] md:w-[400px] mt-8 md:mt-16 items-center justify-between">
+    <div className="flex w-[80%] md:w-[400px] mt-8 md:mt-12 items-center justify-between">
       <div className={`${gameInPlay && OtoPlay && "opacity-20"} flex items-center`}>
         <div className="text-lg md:text-2xl">{gameData.x ? gameData.x : "Waiting..."}</div>
-        <div className="text-7xl md:text-9xl opacity-20 -ml-5 md:-ml-8">X</div>
+        <div className="text-5xl md:text-7xl opacity-20 -ml-5 md:-ml-8">X</div>
       </div>
       <div className="text-red-600">
         {gameDraw && "Draw"}
-        {X_Won && "X Won"}
-        {O_Won && "O Won"}
+        {X_Won && `${gameData.x}(x) Won`}
+        {O_Won && `${gameData.o}(o) Won`}
         {gameInPlay && "-VS-"}
       </div>
       <div className={`${gameInPlay && XtoPlay && "opacity-20"} flex items-center`}>
-        <div className="text-7xl md:text-9xl opacity-20 -mr-5 md:-mr-8">O</div>
+        <div className="text-5xl md:text-7xl opacity-20 -mr-5 md:-mr-8">O</div>
         <div className="text-lg md:text-2xl">{gameData.o ? gameData.o : "Waiting..."}</div>
       </div>
     </div>
